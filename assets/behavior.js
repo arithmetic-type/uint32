eee2b800ff3c5db490708b67618be2a76c789abc
aureooms <aurelien.ooms@gmail.com>
2014-11-24 16:03:19 +0100

    Generated documentation for c260e48a5c58b13f31129ddef7bc18a05312a074

--- assets/behavior.js
+++ assets/behavior.js
@@ -91,6 +91,26 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
       },
       "depth": 1,
       "outline": []
+    }, {
+      "type": "file",
+      "data": {
+        "language": {
+          "nameMatchers": [".js"],
+          "pygmentsLexer": "javascript",
+          "multiLineComment": ["/*", "*", "*/"],
+          "singleLineComment": ["//"],
+          "ignorePrefix": "}",
+          "foldPrefix": "^",
+          "name": "JavaScript"
+        },
+        "sourcePath": "/home/aureooms/dev/js/js-uint32/js/src/usqrt.js",
+        "projectPath": "js/src/usqrt.js",
+        "targetPath": "usqrt",
+        "pageTitle": "usqrt",
+        "title": "usqrt"
+      },
+      "depth": 1,
+      "outline": []
     }
   ];
 
